9f491e170c570e42d070d942edfdeee7c0dcf84e
lauraholguin0 <109982217+lauraholguin0@users.noreply.github.com>
2022-09-24 16:56:26 -0500

    Update constantes.py

--- Damas/damas/constantes.py
+++ Damas/damas/constantes.py
@@ -7,11 +7,10 @@ filas,colu=8,8 #NUMERO FILAS COLUMNAS
 t_cuadrado=width//colu
 
 #COLORES
-cafe_oscuro=(124, 67, 32)
-cafe_claro=(218, 178, 84)
+negro=(0,0,0)
 rojo=(255,0,0)
 azul=(0,0,255)
 blanco=(255,255,255)
 gris=(128,128,128)
-negro=(0,0,0)
+
 corona=pygame.transform.scale(pygame.image.load('damas/crown.png'),(44,25)) #IMAGEN DEL REY 
